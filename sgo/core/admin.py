e5cdb106a800dda37f0fb2d64bb4fd216ad47e6a
RodrigoSantosDev82 <166881134+RodrigoSantosDev82@users.noreply.github.com>
2025-04-10 14:42:58 -0300

    first commit

---
+++ sgo/core/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Funcao, Exame, TipoExame, TipoRisco, Risco, Grupo, Treinamento
+
+# Register your models here.
+admin.site.register(Funcao)
+admin.site.register(Exame)
+admin.site.register(TipoExame)
+admin.site.register(TipoRisco)
+admin.site.register(Risco)
+admin.site.register(Grupo)
+admin.site.register(Treinamento)
